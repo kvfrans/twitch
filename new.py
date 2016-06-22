240333267aaa9119da40aa83ded04a8ec2713a6b
Kevin Frans <kevin@bobthechicken.com>
2016-06-22 14:40:57 -0700

    Asd

--- new.py
+++ new.py
@@ -151,6 +151,7 @@ def classify():
         word = words[l].lower()
         if word in wordsdata:
             print word
+            print wordsdata[word]
             wordarray[:,l,wordsdata[word]] = 1
     preds = sess.run([model.prediction], {data: wordarray, dropout: 1})
     preds = preds[0]
