6cbd960a441a4f9268d31e149b513afcd9e5cf5e
neoblizz <osama94@gmail.com>
2017-03-06 10:01:33 -0800

    Updated for tensorflow api 1.0.0

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ batchsize = 64
 numsteps = 20
 embedsize = 200
 numlayers = 2
-vocabsize = 33235
+vocabsize = 33235 # WORDS + 2 IF YOU ARE USING YOUR OWN DATABASE: CHANGE THIS!!!
 keep_prob = 0.5
 
 input_data = tf.placeholder(tf.int32,[batchsize,numsteps])
 targets = tf.placeholder(tf.int32,[batchsize,numsteps])
-lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(embedsize)
-lstm_cell = tf.nn.rnn_cell.DropoutWrapper(lstm_cell,keep_prob)
-cell = tf.nn.rnn_cell.MultiRNNCell([lstm_cell] * numlayers)
+lstm_cell = tf.contrib.rnn.BasicLSTMCell(embedsize) # tf.nn.rnn_cell.BasicLSTMCell(embedsize)
+lstm_cell = tf.contrib.rnn.DropoutWrapper(lstm_cell,keep_prob) # tf.nn.rnn_cell.DropoutWrapper(lstm_cell,keep_prob)
+cell = tf.contrib.rnn.MultiRNNCell([lstm_cell] * numlayers) # tf.nn.rnn_cell.MultiRNNCell([lstm_cell] * numlayers)
 
 initialstate = cell.zero_state(batchsize, tf.float32)
 
@@ -22,14 +22,14 @@ with tf.variable_scope("rnnlm"):
     with tf.device("/cpu:0"):
         embedding = tf.get_variable("embedding", [vocabsize, embedsize])
         # inputs = list of 20, each containing [64,200]
-        inputs = tf.split(1, numsteps, tf.nn.embedding_lookup(embedding, input_data))
+        inputs = tf.split(tf.nn.embedding_lookup(embedding, input_data), numsteps, 1) # inputs = tf.split(1, numsteps, tf.nn.embedding_lookup(embedding, input_data))
         inputs = [tf.squeeze(input_, [1]) for input_ in inputs]
 
     softmax_w = tf.get_variable("softmax_w", [embedsize, vocabsize])
     softmax_b = tf.get_variable("softmax_b", [vocabsize])
 
 # this basically runs through an RNN, putting a word in at each timestep
-outputs, last_state = tf.nn.seq2seq.rnn_decoder(inputs, initialstate, cell, loop_function=None, scope='rnnlm')
+outputs, last_state = tf.contrib.legacy_seq2seq.rnn_decoder(inputs, initialstate, cell, loop_function=None, scope='rnnlm')
 # outputs: list(20), of [batchsize x outputsize], [64 x 200]
 # outputs are word embeddings
 
@@ -38,7 +38,7 @@ outputs, last_state = tf.nn.seq2seq.rnn_decoder(inputs, initialstate, cell, loop
 # predictions = tf.matmul( tf.concat(2,outputs), softmax_w) + softmax_b
 
 # put all the timesteps/batches in one dimension
-output = tf.reshape(tf.concat(1,outputs), [-1, embedsize])
+output = tf.reshape(tf.concat(axis=1,values=outputs), [-1, embedsize])
 
 # word embeddings to word probabilities
 logits = tf.matmul(output, softmax_w) + softmax_b
@@ -48,7 +48,9 @@ probs = tf.nn.softmax(logits)
 flat_targets = tf.reshape(targets, [-1])
 # sequence by loss requires a weighting for each word, use tf.ones so they are all weighted equal
 weights = tf.ones([batchsize * numsteps])
-loss = tf.nn.seq2seq.sequence_loss_by_example([logits], [flat_targets], [weights])
+
+loss = tf.contrib.legacy_seq2seq.sequence_loss_by_example([logits], [flat_targets], [weights])
+# loss = tf.nn.seq2seq.sequence_loss_by_example([logits], [flat_targets], [weights])
 cost = tf.reduce_sum(loss) / (batchsize * numsteps)
 final_state = last_state
 learningrate = tf.Variable(0.0, trainable=False)
